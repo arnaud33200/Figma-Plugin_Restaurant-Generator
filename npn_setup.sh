f3e4f415f3ad6b2a023c47c1efc6e51eac6a7339
Arnaud Ladoucette <arnaud@ritual.co>
2021-11-27 11:45:44 -0500

    try to setup webpack

--- npn_setup.sh
+++ npn_setup.sh
@@ -1,3 +1,3 @@
 npm install -g typescript
 npm install --save-dev @figma/plugin-typings
-
+npm install --save-dev css-loader html-webpack-inline-source-plugin@beta html-webpack-plugin style-loader ts-loader typescript url-loader webpack webpack-cli
